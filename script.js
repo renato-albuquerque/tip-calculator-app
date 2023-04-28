cd8ffe755214e2a531685c12d35fe3788b5fca4b
renato <renatomalb83@gmail.com>
2023-04-28 11:27:56 -0300

    updated the variables in js file

--- script.js
+++ script.js
@@ -6,10 +6,18 @@ const btn10 = document.querySelector("#btn10");
 const btn15 = document.querySelector("#btn15");
 const btn25 = document.querySelector("#btn25");
 const btn50 = document.querySelector("#btn50");
-const inputCustom = document.querySelector(".inputCustom")
+const inputCustom = document.querySelector(".inputCustom");
 const inputPeople = document.querySelector("#qtyPerPerson");
 const errorMessage = document.querySelector("#alertMessage");
 const tipResult = document.querySelector("#tipResult"); 
 const totalResult = document.querySelector("#totalResult");
-const btnReset = document.querySelector(".buttonReset")
+const btnReset = document.querySelector(".buttonReset");
+
+let billAmount = Number(inputBill.value)
+let customPercent = Number(inputCustom.value)
+let numberPeople = Number(inputPeople.value)
+
+
+
+
 

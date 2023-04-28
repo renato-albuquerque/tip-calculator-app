455a4fc7b4af7972b2db54759fcb83a2389d6cd9
renato <renatomalb83@gmail.com>
2023-04-27 23:42:46 -0300

    started js code

---
+++ script.js
@@ -0,0 +1,14 @@
+let billValue = document.querySelector("#billValue")
+let billValue2 = Number(billValue.value)
+
+let qtyPerPerson = document.querySelector("#qtyPerPerson")
+let qtyPerPerson2 = Number(qtyPerPerson.value)
+
+let calc = (billValue2 + qtyPerPerson2)
+
+
+console.log(billValue2);
+console.log(qtyPerPerson2);
+console.log(calc);
+
+
